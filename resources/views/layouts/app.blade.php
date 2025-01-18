9991ae2ed2cc2069f85551bc4c6e786d8b04b33c
Sasanga Chathumal <chathumal.sasanga02@gmail.com>
2025-01-18 09:16:43 +0530

    Added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,9 +28,12 @@
     @if (env('IS_DEMO'))
     <x-demo-metas></x-demo-metas>
     @endif
-
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <link rel="icon" type="image/png" href="../assets/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/svg+xml" href="../assets/favicon/favicon.svg" />
+    <link rel="shortcut icon" href="../assets/favicon/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="../assets/favicon/apple-touch-icon.png" />
+    <meta name="apple-mobile-web-app-title" content="GasByGas" />
+    <link rel="manifest" href="../assets/favicon/site.webmanifest" />
     <title>
         GasByGas
     </title>
--- resources/views/session/login-session.blade.php
+++ resources/views/session/login-session.blade.php
@@ -10,8 +10,8 @@
                     <div class="col-xl-4 col-lg-5 col-md-6 d-flex flex-column mx-auto">
                         <div class="card card-plain mt-8">
                             <div class="card-header pb-0 text-left bg-transparent">
-                                <h3 class="font-weight-bolder text-info text-gradient">Welcome to GasByGas</h3>
-                                <p class="mb-0">Sign in with your credentials:</p>
+                                <h3 class="font-weight-bolder text-warning text-gradient">Welcome to GasByGas</h3>
+                                <p class="mb-0">Sign in with your credentials</p>
                             </div>
                             <div class="card-body">
                                 <form id="login-form" role="form" method="POST">
@@ -44,10 +44,6 @@
                                 <small class="text-muted">Forgot you password? Reset you password
                                     <a href="/login/forgot-password" class="text-info text-gradient font-weight-bold">here</a>
                                 </small>
-                                <p class="mb-4 text-sm mx-auto">
-                                    Don't have an account?
-                                    <a href="register" class="text-info text-gradient font-weight-bold">Sign up</a>
-                                </p>
                             </div>
                         </div>
                     </div>
